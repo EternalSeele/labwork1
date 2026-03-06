2222e439476fae2f6ba643a77730157d645013d8
Vzorov laptop <opera1140@gmail.com>
2026-03-06 06:59:18 +0000

    Changed file Rectangle

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,40 +1,51 @@
 #include <iostream>
 #include <cmath>
 
-int perimeterRectangle(int a, int b) { //Функция для вычисления периметра прямоугольника
-    return (a + b) * 2 ;
+int perimeterRectangle(int a, int b) {
+    return (a + b) * 2;
 }
 
-int areaRectangle(int a, int b) { //Функция для вычисления площади прямоугольника
+int areaRectangle(int a, int b) {
     return a * b;
 }
 
-double diagonalRectangle(int a, int b) { //Функция для вычисления длины диагонали прямоугольника
+double diagonalRectangle(int a, int b) {
     return sqrt(a * a + b * b);
 }
 
-int main() { //Основная функция программы
+int main() {
     std::cout << "Выберите действие с прямоугольником:" << std::endl;
     std::cout << "1) Вычисление периметра" << std::endl;
     std::cout << "2) Вычисление площади" << std::endl;
     std::cout << "3) Вычисление длины диагонали" << std::endl;
+    
     int num;
     std::cin >> num;
 
     std::cout << "Введите значение длины стороны a:" << std::endl;
     int a;
     std::cin >> a;
+    if (a <= 0) {
+        std::cout << "Used wrong number" << std::endl;
+        return 1;
+    }
 
     std::cout << "Введите значение длины стороны b:" << std::endl;
     int b;
     std::cin >> b;
-
-    if (num == 1) {
-        std::cout <<"Периметр равен:"<< perimeterRectangle(a, b);
-    } else if (num == 2) {
-        std::cout <<"Площадь равна:"<< areaRectangle(a, b);
-    } else if (num == 3) {
-        std::cout <<"Длина диагонали равна:"<< diagonalRectangle(a, b);
+    if (b <= 0) {
+        std::cout << "Used wrong number" << std::endl;
+        return 1;
     }
 
+    if (num == 1) {
+        std::cout << "Периметр равен: " << perimeterRectangle(a, b) << std::endl;
+    } else if (num == 2) {
+        std::cout << "Площадь равна: " << areaRectangle(a, b) << std::endl;
+    } else if (num == 3) {
+        std::cout << "Длина диагонали равна: " << diagonalRectangle(a, b) << std::endl;
+    }
+    
+    return 0; // Успешное завершение
 }
+
